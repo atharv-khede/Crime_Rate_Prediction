a3f5508a48cc37d16a5114a0d4d76f9c10a88e06
atharv-khede <atharvkhede625@gmail.com>
2023-10-09 18:20:23 +0530

    added static url

--- CrimeRate/settings.py
+++ CrimeRate/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'  
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('authentication/static')),)
+STATIC_URL = '/authentication/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
