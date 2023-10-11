64b8f358521c926d0f989c5235e7829e9f03a3c5
atharv-khede <atharvkhede625@gmail.com>
2023-10-11 14:31:14 +0530

    update host settings

--- CrimeRate/settings.py
+++ CrimeRate/settings.py
@@ -124,6 +124,7 @@ STATICFILES_DIRS = (str(BASE_DIR.joinpath('/static')),)
 STATIC_URL = '/static/'
 
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
