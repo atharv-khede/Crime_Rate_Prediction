188bf2a672e5647f719dfdde52a621762b12821f
atharv-khede <atharvkhede625@gmail.com>
2023-10-11 15:33:38 +0530

    first commit

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    settings_module = 'CrimeRate.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'CriemRate.settings'
+    settings_module = 'CrimeRate.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'CrimeRate.settings'
 
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
